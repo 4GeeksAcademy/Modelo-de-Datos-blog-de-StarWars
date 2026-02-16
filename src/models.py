cd7670c16c9a8d437cbf5e3545f81e9f2a476b89
Lorena Acosta <lorena07acosta@gmail.com>
2026-02-16 23:58:19 +0000

    Modelo de Datos del blog de StarWars

--- src/models.py
+++ src/models.py
@@ -1,19 +1,59 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
 
 db = SQLAlchemy()
 
 class User(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
-
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+    # Relación: Un usuario puede tener muchos favoritos
+    favorites = db.relationship('Favorite', backref='user', lazy=True)
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            "username": self.username,
+            "email": self.email
+        }
+
+class People(db.Model):
+    __tablename__ = 'people'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    height = db.Column(db.String(20))
+    mass = db.Column(db.String(20))
+    # Relación: Un personaje puede ser favorito de muchos usuarios
+    favorites = db.relationship('Favorite', backref='people', lazy=True)
+
+    def serialize(self):
+        return {"id": self.id, "name": self.name}
+
+class Planet(db.Model):
+    __tablename__ = 'planet'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    climate = db.Column(db.String(80))
+    population = db.Column(db.String(80))
+    # Relación: Un planeta puede ser favorito de muchos usuarios
+    favorites = db.relationship('Favorite', backref='planet', lazy=True)
+
+    def serialize(self):
+        return {"id": self.id, "name": self.name}
+
+class Favorite(db.Model):
+    __tablename__ = 'favorite'
+    id = db.Column(db.Integer, primary_key=True)
+    # Claves Foráneas: Conectan el favorito con el Usuario, el Personaje o el Planeta
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    people_id = db.Column(db.Integer, db.ForeignKey('people.id'), nullable=True)
+    planet_id = db.Column(db.Integer, db.ForeignKey('planet.id'), nullable=True)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "people_id": self.people_id,
+            "planet_id": self.planet_id
         }
